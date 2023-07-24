079e26712119f7bfe620addad577fc361bd5287f
Schäffer Krisztián <tisztamo@nore.ply>
2023-07-24 12:35:18 +0200

    Install xterm.js and implement in ExecutionResultDisplay https://chat.openai.com/share/13353c8b-422d-40dc-8e95-15ab1f83cf4d

--- src/frontend/components/ExecutionResultDisplay.jsx
+++ src/frontend/components/ExecutionResultDisplay.jsx
@@ -1,10 +1,25 @@
+import { onMount, createEffect } from 'solid-js';
+import { Terminal } from 'xterm';
+import 'xterm/css/xterm.css';
 import { executionResult } from '../stores/executionResult';
 
 const ExecutionResultDisplay = () => {
+  let container;
+  let term;
+
+  onMount(() => {
+    term = new Terminal();
+    term.open(container);
+  });
+
+  createEffect(() => {
+    if (term) {
+      term.write(executionResult());
+    }
+  });
+
   return (
-    <div class="w-64 px-4 py-4 bg-gray-300 text-black rounded">
-      {executionResult()}
-    </div>
+    <div ref={container} class="w-64 px-4 py-4 bg-gray-300 text-black rounded"></div>
   );
 };
 
