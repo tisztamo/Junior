27ded9c96d40411d6c4ff1612bd4d6675e36f69e
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-07-30 22:24:53 +0200

    Add change.sh. This and previous commit: https://chat.openai.com/share/8d21b3a3-5e9e-416d-8205-d13351d7358b

--- change.sh
+++ change.sh
@@ -1,81 +1,21 @@
 #!/bin/sh
 set -e
-goal="Rename project from 'The Junior' to 'Junior'"
+goal="Revert src/frontend/ to original state"
 echo "Plan:"
-echo "1. Rename all occurrences of 'The Junior' or 'Contributor' to 'Junior' in README.md file."
-echo "2. Verify that the changes are applied correctly."
+echo "1. Identify the common ancestor commit with the main branch (or another specific branch)"
+echo "2. Checkout the src/frontend/ directory at that commit"
+echo "3. Commit the changes to revert the src/frontend/ directory to its original state"
 
-# Step 1
-cat > README.md << 'EOF'
-Warn: This README is AI generated, just like all the source files of this project.
+# Step 1: Identify the common ancestor commit with the main branch (replace 'main' with the specific branch if needed)
+branch_creation_commit=$(git merge-base HEAD main)
 
-# Junior - Your AI contributor which writes itself.
+echo "Branch creation commit: $branch_creation_commit"
 
-## Description
+# Step 2: Checkout the src/frontend/ directory at that commit
+git checkout $branch_creation_commit -- src/frontend/
 
-Junior is an exploratory project aimed at revolutionizing the way programmers interact with the development process. Just like how Linus Torvalds oversees the Linux Kernel development without coding himself, this project allows developers to communicate with the AI and supervise the development process.
-
-By providing specific task details in a prompt descriptor and highlighting the relevant parts of your project, you can delegate code implementation, documentation, testing, and more to your AI Junior.
-
-## Getting Started
-
-### Installation
-
-To install, clone the repository and run `npm install` in the root directory.
-
-### Usage
-
-There are two ways to use this project: a command-line interface (CLI) and a web interface.
-
-#### Command-line interface (CLI)
-
-To start the CLI, use `npm run cli`. This mode uses the ChatGPT API, and you'll need an API key stored in the `OPENAI_API_KEY` environment variable.
-
-#### Web Interface
-
-Run the application with `npm start` to start a local server on port 3000, where you can generate a prompt and automatically copy it to paste into ChatGPT. The web interface is designed for use with ChatGPT Pro and doesn't require an API key.
-
-### The Prompt Descriptor
-
-A prompt descriptor is a YAML file (`prompt.yaml`) outlining the details necessary for generating a task prompt for the AI model.
-
-Here's an example of a prompt descriptor:
-
-```yaml
-task: prompt/task/feature/implement.md
-attention:
-  - src/interactiveSession/startInteractiveSession.js
-  - src/prompt/createPrompt.js
-  - src/attention/readAttention.js
-  - prompt.yaml
-requirements: >
-  Write a README.md for this _exploratory_ project!
-format: prompt/format/new_file_version.md
-```
-
-Each element in the descriptor serves a specific purpose:
-- `task`: Describes the task type and scope. For example, `feature/implement`, `bug/fix`, or `refactor/`. You can check out the [prompt/task/feature/implement.md](prompt/task/feature/implement.md) file as an example.
-- `attention`: Lists the files and directories most relevant to the task.
-- `requirements`: Describes the actual task in a human-readable format.
-- `format`: Determines how the output will be formatted.
-
-### Attention Mechanism
-
-The attention mechanism guides the AI model by providing it with a working set. It helps overcome the limited working memory of large language models.
-
-The working set is a subset of the entire project that's currently in focus. It includes both files and directories. For files, the content is directly provided to the AI. For directories, a brief list of files and subdirectories within them is presented.
-
-## Contributing and Support
-
-Contributions are welcome! Remember, we eat our own dog food in this project. Junior is designed to write itself. Your main role will be to oversee the work, provide detailed prompts, and review the outcomes.
-
-For support, please create an issue in the GitHub repository.
-
-**Note:** For meaningful results, it's recommended to use the GPT-4 model or a more recent version.
-EOF
-
-# Step 2
-echo "Verifying the changes:"
-grep -q "Junior" README.md && echo "\033[32mChange successfully applied to README.md\033[0m" || echo "\033[31mChange not applied correctly to README.md\033[0m"
+# Step 3: Commit the changes
+git add src/frontend/
+git commit -m "Revert src/frontend/ to its state at branch creation"
 
 echo "\033[32mDone: $goal\033[0m\n"
