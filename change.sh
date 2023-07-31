7dca4ff841a8b89b619aaffe21dbba66acf4f131
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-07-31 12:57:31 +0200

    Generate logo, favicon, and update HTML https://chat.openai.com/share/818d5139-d5a2-48ec-80a7-9d2d223ea729

--- change.sh
+++ change.sh
@@ -1,83 +1,43 @@
 #!/bin/sh
 set -e
-goal="Improve documentation in README.md"
+goal="Generate logo, favicon, and update HTML"
 echo "Plan:"
-echo "1. Fix the video caption by adding a newline."
-echo "2. Extend the description to explain the difference with ChatGPT."
-echo "3. Remove port information and link to doc/web.md for more info."
+echo "1. Generate a logo with four rounded rectangles"
+echo "2. Save the logo to doc/assets/logo.png"
+echo "3. Create a favicon based on the logo"
+echo "4. Create the directory src/frontend/assets/ if not exists"
+echo "5. Save the favicon to src/frontend/assets/"
+echo "6. Update src/frontend/index.html to use the favicon"
 
-# Step 1: Fix the video caption by adding a newline
-cat << 'EOF' > README.md
-Warn: This README is AI generated, just like all the source files of this project.
+# Step 1 & 2: Generate and save the logo
+convert -size 100x100 xc:white \
+    -fill "rgb(59, 130, 246)" -draw "roundrectangle 0,0 100,24 10,10" \
+    -fill "rgb(253, 186, 116)" -draw "roundrectangle 0,26 100,49 10,10" \
+    -fill "rgb(185, 28, 28)" -draw "roundrectangle 0,51 100,74 10,10" \
+    -fill "rgb(28, 185, 28)" -draw "roundrectangle 0,76 100,100 10,10" \
+    doc/assets/logo.png
 
-# Junior - Your AI contributor which writes itself.
+# Step 4: Create the directory if not exists
+mkdir -p src/frontend/assets/
 
-[![Video: Junior codes itself](doc/assets/video_cover.jpg)](https://www.youtube.com/watch?v=W_iwry8uT7E)
+# Step 3: Create favicon
+convert doc/assets/logo.png -resize 32x32 src/frontend/assets/favicon.ico
 
-*"Video: Junior codes itself"*
-## Description
-
-Junior is an exploratory project aimed at revolutionizing the way programmers interact with the development process. Just like how Linus Torvalds oversees the Linux Kernel development without coding himself, this project allows developers to communicate with the AI and supervise the development process.
-
-Isn't that already possible with ChatGPT? No, LLMs have very limited "working memory", so it is not possible to directly work with them on large codebases.
-
-By providing specific task details in a prompt descriptor and highlighting the relevant parts of your project, you can delegate code implementation, documentation, testing, and more to your AI Junior.
-
-## Getting Started
-
-### Installation
-
-To install, clone the repository and run `npm install` in the root directory.
-
-### Usage
-
-There are two ways to use this project: a command-line interface (CLI) and a web interface.
-
-#### Command-line interface (CLI)
-
-To start the CLI, use `npm run cli`. This mode uses the ChatGPT API, and you'll need an API key stored in the `OPENAI_API_KEY` environment variable.
-
-#### Web Interface
-
-Run the application with `npm start` to start a local server, where you can generate a prompt and automatically copy it to paste into ChatGPT. The web interface is designed for use with ChatGPT Pro and doesn't require an API key. For more information about the web interface, please refer to [doc/web.md](doc/web.md).
-
-### The Prompt Descriptor
-
-A prompt descriptor is a YAML file (`prompt.yaml`) outlining the details necessary for generating a task prompt for the AI model.
-
-Here's an example of a prompt descriptor:
-
-```yaml
-task: prompt/task/feature/implement.md
-attention:
-  - src/interactiveSession/startInteractiveSession.js
-  - src/prompt/createPrompt.js
-  - src/attention/readAttention.js
-  - prompt.yaml
-requirements: >
-  Write a README.md for this _exploratory_ project!
-format: prompt/format/new_file_version.md
-```
-
-Each element in the descriptor serves a specific purpose:
-- `task`: Describes the task type and scope. For example, `feature/implement`, `bug/fix`, or `refactor/`. You can check out the [prompt/task/feature/implement.md](prompt/task/feature/implement.md) file as an example.
-- `attention`: Lists the files and directories most relevant to the task.
-- `requirements`: Describes the actual task in a human-readable format.
-- `format`: Determines how the output will be formatted.
-
-### Attention Mechanism
-
-The attention mechanism guides the AI model by providing it with a working set. It helps overcome the limited working memory of large language models.
-
-The working set is a subset of the entire project that's currently in focus. It includes both files and directories. For files, the content is directly provided to the AI. For directories, a brief list of files and subdirectories within them is presented.
-
-## Contributing and Support
-
-Contributions are welcome! Remember, we eat our own dog food in this project. Junior is designed to write itself. Your main role will be to oversee the work, provide detailed prompts, and review the outcomes.
-
-For support, please create an issue in the GitHub repository.
-
-**Note:** For meaningful results, it's recommended to use the GPT-4 model or a more recent version.
+# Step 6: Update index.html to use the favicon
+cat > src/frontend/index.html << 'EOF'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <link rel="icon" href="/assets/favicon.ico" type="image/x-icon">
+  <title>Junior</title>
+</head>
+<body>
+  <div id="app"></div>
+  <script type="module" src="/index.jsx"></script>
+</body>
+</html>
 EOF
 
 echo "\033[32mDone: $goal\033[0m\n"
--- prompt.md
+++ prompt.md
@@ -1,74 +1,62 @@
 # Working set
 
-README.md:
 ```
-Warn: This README is AI generated, just like all the source files of this project.
+./
+├── .DS_Store
+├── .git/...
+├── .github/...
+├── .gitignore
+├── .vscode/...
+├── README.md
+├── change.sh
+├── doc/...
+├── integrations/...
+├── node_modules/...
+├── package-lock.json
+├── package.json
+├── prompt/...
+├── prompt.md
+├── prompt.yaml
+├── src/...
 
-# Junior - Your AI contributor which writes itself.
-
-[![Video: Junior codes itself](doc/assets/video_cover.jpg)](https://www.youtube.com/watch?v=W_iwry8uT7E)
-*"Video: Junior codes itself"*
-## Description
-
-Junior is an exploratory project aimed at revolutionizing the way programmers interact with the development process. Just like how Linus Torvalds oversees the Linux Kernel development without coding himself, this project allows developers to communicate with the AI and supervise the development process.
-
-By providing specific task details in a prompt descriptor and highlighting the relevant parts of your project, you can delegate code implementation, documentation, testing, and more to your AI Junior.
-
-## Getting Started
-
-### Installation
-
-To install, clone the repository and run `npm install` in the root directory.
-
-### Usage
-
-There are two ways to use this project: a command-line interface (CLI) and a web interface.
-
-#### Command-line interface (CLI)
-
-To start the CLI, use `npm run cli`. This mode uses the ChatGPT API, and you'll need an API key stored in the `OPENAI_API_KEY` environment variable.
-
-#### Web Interface
-
-Run the application with `npm start` to start a local server on port 3000, where you can generate a prompt and automatically copy it to paste into ChatGPT. The web interface is designed for use with ChatGPT Pro and doesn't require an API key.
-
-### The Prompt Descriptor
-
-A prompt descriptor is a YAML file (`prompt.yaml`) outlining the details necessary for generating a task prompt for the AI model.
-
-Here's an example of a prompt descriptor:
-
-```yaml
-task: prompt/task/feature/implement.md
-attention:
-  - src/interactiveSession/startInteractiveSession.js
-  - src/prompt/createPrompt.js
-  - src/attention/readAttention.js
-  - prompt.yaml
-requirements: >
-  Write a README.md for this _exploratory_ project!
-format: prompt/format/new_file_version.md
 ```
+```
+doc/assets/
+├── video_cover.jpg
 
-Each element in the descriptor serves a specific purpose:
-- `task`: Describes the task type and scope. For example, `feature/implement`, `bug/fix`, or `refactor/`. You can check out the [prompt/task/feature/implement.md](prompt/task/feature/implement.md) file as an example.
-- `attention`: Lists the files and directories most relevant to the task.
-- `requirements`: Describes the actual task in a human-readable format.
-- `format`: Determines how the output will be formatted.
+```
+```
+src/frontend/
+├── App.jsx
+├── components/...
+├── fetchTasks.js
+├── generatePrompt.js
+├── getBaseUrl.js
+├── index.html
+├── index.jsx
+├── postcss.config.cjs
+├── service/...
+├── startVite.js
+├── stores/...
+├── styles/...
+├── tailwind.config.cjs
+├── vite.config.js
 
-### Attention Mechanism
-
-The attention mechanism guides the AI model by providing it with a working set. It helps overcome the limited working memory of large language models.
-
-The working set is a subset of the entire project that's currently in focus. It includes both files and directories. For files, the content is directly provided to the AI. For directories, a brief list of files and subdirectories within them is presented.
-
-## Contributing and Support
-
-Contributions are welcome! Remember, we eat our own dog food in this project. Junior is designed to write itself. Your main role will be to oversee the work, provide detailed prompts, and review the outcomes.
-
-For support, please create an issue in the GitHub repository.
-
-**Note:** For meaningful results, it's recommended to use the GPT-4 model or a more recent version.
+```
+src/frontend/index.html:
+```
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>Junior</title>
+</head>
+<body>
+  <div id="app"></div>
+  <script type="module" src="/index.jsx"></script>
+</body>
+</html>
 
 ```
 
@@ -77,10 +65,7 @@ For support, please create an issue in the GitHub repository.
 
 Improve the documentation!
 
-- Fix: The video caption is displayed on the same line as the video. Add an extra newline!
-- Extend the description in readme with the following (put it between the two paragraphs, reword if needed): Isn&#39;t that already possible with ChatGPT? No, LLMs have very limited &#34;working memory&#34;, so it is not possible to directly work with them on large codebases.
-- Remove port info from README and link doc/web.md for more info
-heredoc whole README.md!
+Generate a logo to doc/assets/logo.png The logo is 1:1 consists of 4 rounded rectangles stacked on each other with small gaps inbetween. Colors from up to down: rgb(59, 130, 246), rgb(253, 186, 116), rgb(185, 28, 28) and rgb(28, 185, 28) generate a favicon to src/frontend/assets/ (Create directory) Use the favicon in index.html convert is installed
 
 
 # Output Format
