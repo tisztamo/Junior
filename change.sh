e2478c4a83ae7766a8bf2db42698832b19dc5ca2
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-08-06 21:17:41 +0200

    Create and replace logo file https://chat.openai.com/share/c94d2481-7aaf-45a7-b841-f5d691d9d5f5

--- change.sh
+++ change.sh
@@ -1,80 +1,25 @@
 #!/bin/sh
 set -e
-goal="Update doc/ references to docs/"
+goal="Create and replace logo file"
 echo "Plan:"
-echo "1. Rename doc/ references in README.md"
-echo "2. Rename doc/ references in the attention section of the prompt descriptor example"
+echo "1. Create the SVG logo file with the given specifications."
+echo "2. Convert the SVG file to PNG using 'convert'."
+echo "3. Overwrite the old PNG logo file."
 
-cat > ./README.md << 'EOF'
-Warn: This README is AI generated, just like all the source files of this project.
-
-# Junior - Your AI contributor which codes itself.
-
-[![Video: Junior codes itself](docs/assets/video_cover.jpg)](https://www.youtube.com/watch?v=W_iwry8uT7E)
-
-*"Video: Junior codes itself"*
-## Description
-
-Junior is an exploratory project aimed at revolutionizing the way programmers interact with the development process. Just like how Linus Torvalds oversees the Linux Kernel development without coding himself, this project allows developers to communicate with the AI and supervise the development process.
-
-Isn't that already possible with ChatGPT? No, LLMs have very limited "working memory", so it is not possible to directly work with them on large codebases.
-
-By providing specific task details in a prompt descriptor and highlighting the relevant parts of your project, you can delegate code implementation, documentation, testing, and more to your AI Junior.
-
-## Getting Started
-
-### Installation
-
-To install, clone the repository and run `npm install` in the root directory. Additionally, you can install the "Junior" vscode extension from the vscode extension marketplace.
-
-### Usage
-
-#### Web Interface
-
-Run the application with `npm start` to start a local server, where you can generate a prompt and automatically copy it to paste into ChatGPT. The web interface is designed for use with ChatGPT Pro and doesn't require an API key. For more information about the web interface, please refer to [docs/web.md](docs/web.md).
-
-#### Command-line interface (CLI)
-
-To start the CLI, use `npm run cli`. This mode uses the ChatGPT API, and you'll need an API key stored in the `OPENAI_API_KEY` environment variable.
-
-### The Prompt Descriptor
-
-A prompt descriptor is a YAML file (`prompt.yaml`) outlining the details necessary for generating a task prompt for the AI model.
-
-Here's an example of a prompt descriptor:
-
-```yaml
-task: prompt/task/feature/implement.md
-attention:
-  - src/interactiveSession/startInteractiveSession.js
-  - src/prompt/createPrompt.js
-  - src/attention/readAttention.js
-  - prompt.yaml
-requirements: >
-  Write a README.md for this _exploratory_ project!
-format: prompt/format/new_file_version.md
-```
-
-Each element in the descriptor serves a specific purpose:
-- `task`: Describes the task type and scope. For example, `feature/implement`, `bug/fix`, or `refactor/`. You can check out the [prompt/task/feature/implement.md](prompt/task/feature/implement.md) file as an example.
-- `attention`: Lists the files and directories most relevant to the task.
-- `requirements`: Describes the actual task in a human-readable format.
-- `format`: Determines how the output will be formatted.
-
-### Attention Mechanism
-
-The attention mechanism guides the AI model by providing it with a working set. It helps overcome the limited working memory of large language models.
-
-The working set is a subset of the entire project that's currently in focus. It includes both files and directories. For files, the content is directly provided to the AI. For directories, a brief list of files and subdirectories within them is presented.
-
-## Contributing and Support
-
-Contributions are welcome! Remember, we eat our own dog food in this project. Junior is designed to write itself. Your main role will be to oversee the work, provide detailed prompts, and review the outcomes.
-
-For support, please create an issue in the GitHub repository.
-
-**Note:** For meaningful results, it's recommended to use the GPT-4 model or a more recent version.
-
+# Step 1: Create the SVG logo file with the given specifications.
+cat > ./docs/assets/logo.svg << 'EOF'
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <rect x="0" y="0" rx="10" ry="10" width="100" height="30" style="fill:blue;" />
+    <rect x="0" y="33" rx="10" ry="10" width="100" height="30" style="fill:orange;" />
+    <rect x="0" y="66" rx="10" ry="10" width="48" height="34" style="fill:red;" />
+    <rect x="52" y="66" rx="10" ry="10" width="48" height="34" style="fill:green;" />
+</svg>
 EOF
+
+# Step 2: Convert the SVG file to PNG using 'convert'.
+convert ./docs/assets/logo.svg ./docs/assets/logo.png
+
+# Step 3: Overwrite the old PNG logo file.
+mv -f ./docs/assets/logo.png ./docs/assets/logo.png
 
 echo "\033[32mDone: $goal\033[0m\n"
--- prompt.md
+++ prompt.md
@@ -1,79 +1,31 @@
 # Working set
 
-./README.md:
 ```
-Warn: This README is AI generated, just like all the source files of this project.
-
-# Junior - Your AI contributor which codes itself.
-
-[![Video: Junior codes itself](doc/assets/video_cover.jpg)](https://www.youtube.com/watch?v=W_iwry8uT7E)
-
-*"Video: Junior codes itself"*
-## Description
-
-Junior is an exploratory project aimed at revolutionizing the way programmers interact with the development process. Just like how Linus Torvalds oversees the Linux Kernel development without coding himself, this project allows developers to communicate with the AI and supervise the development process.
-
-Isn't that already possible with ChatGPT? No, LLMs have very limited "working memory", so it is not possible to directly work with them on large codebases.
-
-By providing specific task details in a prompt descriptor and highlighting the relevant parts of your project, you can delegate code implementation, documentation, testing, and more to your AI Junior.
-
-## Getting Started
-
-### Installation
-
-To install, clone the repository and run `npm install` in the root directory. Additionally, you can install the "Junior" vscode extension from the vscode extension marketplace.
-
-### Usage
-
-#### Web Interface
-
-Run the application with `npm start` to start a local server, where you can generate a prompt and automatically copy it to paste into ChatGPT. The web interface is designed for use with ChatGPT Pro and doesn't require an API key. For more information about the web interface, please refer to [doc/web.md](doc/web.md).
-
-#### Command-line interface (CLI)
-
-To start the CLI, use `npm run cli`. This mode uses the ChatGPT API, and you'll need an API key stored in the `OPENAI_API_KEY` environment variable.
-
-### The Prompt Descriptor
-
-A prompt descriptor is a YAML file (`prompt.yaml`) outlining the details necessary for generating a task prompt for the AI model.
-
-Here's an example of a prompt descriptor:
-
-```yaml
-task: prompt/task/feature/implement.md
-attention:
-  - src/interactiveSession/startInteractiveSession.js
-  - src/prompt/createPrompt.js
-  - src/attention/readAttention.js
-  - prompt.yaml
-requirements: >
-  Write a README.md for this _exploratory_ project!
-format: prompt/format/new_file_version.md
-```
-
-Each element in the descriptor serves a specific purpose:
-- `task`: Describes the task type and scope. For example, `feature/implement`, `bug/fix`, or `refactor/`. You can check out the [prompt/task/feature/implement.md](prompt/task/feature/implement.md) file as an example.
-- `attention`: Lists the files and directories most relevant to the task.
-- `requirements`: Describes the actual task in a human-readable format.
-- `format`: Determines how the output will be formatted.
-
-### Attention Mechanism
-
-The attention mechanism guides the AI model by providing it with a working set. It helps overcome the limited working memory of large language models.
-
-The working set is a subset of the entire project that's currently in focus. It includes both files and directories. For files, the content is directly provided to the AI. For directories, a brief list of files and subdirectories within them is presented.
-
-## Contributing and Support
-
-Contributions are welcome! Remember, we eat our own dog food in this project. Junior is designed to write itself. Your main role will be to oversee the work, provide detailed prompts, and review the outcomes.
-
-For support, please create an issue in the GitHub repository.
-
-**Note:** For meaningful results, it's recommended to use the GPT-4 model or a more recent version.
-
+./
+├── .DS_Store
+├── .git/...
+├── .github/...
+├── .gitignore
+├── .vscode/...
+├── README.md
+├── change.sh
+├── docs/...
+├── integrations/...
+├── node_modules/...
+├── package-lock.json
+├── package.json
+├── prompt/...
+├── prompt.md
+├── prompt.yaml
+├── src/...
 
 ```
+```
+./docs/assets/
+├── logo.png
+├── video_cover.jpg
 
+```
 
 # Task
 
@@ -84,8 +36,12 @@ Implement the following feature!
 
 Requirements:
 
-doc/ was renamed to docs/
-Update references accordingly!
+Create a new 1:1 logo and save it as logo.svg! Then convert to png and overwrite the old one!
+The logo consists of three same-height row stacked on each other with some space inbetween.
+The first row is filled by a blue rounded rectangle
+The second one is filled by an orange rounded rectangle
+The last one contains two rounded rectangles: On the left a red one and on the right a green one. The two fill the row fully and there is some space between them
+convert is installed
 
 
 
