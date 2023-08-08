55b43e13e7ccebe0aa8c14c3ac88abe925a1537b
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-08-08 12:18:54 +0200

    Generate favicon from logo and add to HTML https://chat.openai.com/share/f7c6e88e-7539-4397-aafa-1f050a32b719

--- change.sh
+++ change.sh
@@ -1,61 +1,38 @@
 #!/bin/sh
 set -e
-goal="Update README.md as per requirements"
+goal="Generate favicon from logo and add to HTML"
 echo "Plan:"
-echo "1. Update image URL to /assets/video_cover.jpg"
-echo "2. Remove details in section '### The Prompt Descriptor' and replace with a sentence linking to descriptor.md"
+echo "1. Convert the logo.png to favicon.ico"
+echo "2. Update the index.html file to include the favicon"
 
-# Command to change the image URL
-cat > docs/README.md << 'EOF'
-Warn: This README is AI generated, just like all the source files of this project.
+# Step 1: Convert the logo.png to favicon.ico
+convert docs/assets/logo.png -resize 16x16 docs/assets/favicon.ico
 
-# Junior - Your AI contributor which codes itself.
-
-[![Video: Junior codes itself](/assets/video_cover.jpg)](https://www.youtube.com/watch?v=W_iwry8uT7E)
-
-*"Video: Junior codes itself"*
-## Description
-
-Junior is an exploratory project aimed at revolutionizing the way programmers interact with the development process. Just like how Linus Torvalds oversees the Linux Kernel development without coding himself, this project allows developers to communicate with the AI and supervise the development process.
-
-Isn't that already possible with ChatGPT? No, LLMs have very limited "working memory", so it is not possible to directly work with them on large codebases.
-
-By providing specific task details in a prompt descriptor and highlighting the relevant parts of your project, you can delegate code implementation, documentation, testing, and more to your AI Junior.
-
-## Getting Started
-
-### Installation
-
-To install, clone the repository and run `npm install` in the root directory. Additionally, you can install the "Junior" vscode extension from the vscode extension marketplace.
-
-### Usage
-
-#### Web Interface
-
-Run the application with `npm start` to start a local server, where you can generate a prompt and automatically copy it to paste into ChatGPT. The web interface is designed for use with ChatGPT Pro and doesn't require an API key. For more information about the web interface, please refer to [doc/web.md](doc/web.md).
-
-#### Command-line interface (CLI)
-
-To start the CLI, use `npm run cli`. This mode uses the ChatGPT API, and you'll need an API key stored in the `OPENAI_API_KEY` environment variable.
-
-### The Prompt Descriptor
-
-The prompt descriptor is a YAML file that outlines the details necessary for generating a task prompt for the AI model. For more details, refer to [descriptor.md](descriptor.md).
-
-### Attention Mechanism
-
-The attention mechanism guides the AI model by providing it with a working set. It helps overcome the limited working memory of large language models.
-
-The working set is a subset of the entire project that's currently in focus. It includes both files and directories. For files, the content is directly provided to the AI. For directories, a brief list of files and subdirectories within them is presented.
-
-## Contributing and Support
-
-Contributions are welcome! Remember, we eat our own dog food in this project. Junior is designed to write itself. Your main role will be to oversee the work, provide detailed prompts, and review the outcomes.
-
-For support, please create an issue in the GitHub repository.
-
-**Note:** For meaningful results, it's recommended to use the GPT-4 model or a more recent version.
-
+# Step 2: Update the index.html file to include the favicon
+cat > docs/index.html <<'EOF'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="description" content="Description">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  <link rel="icon" href="assets/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/docsify@4/lib/themes/vue.css">
+</head>
+<body>
+  <style>
+    .app-name-link img {
+      max-width: 70px;
+    }
+  </style>
+  <div id="app"></div>
+  <script src="docsifyConfig.js"></script>
+  <!-- Docsify v4 -->
+  <script src="//cdn.jsdelivr.net/npm/docsify@4"></script>
+</body>
+</html>
 EOF
 
 echo "\033[32mDone: $goal\033[0m\n"
