f5e6b93da18a308157101cf78ff41910f7957ed2
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-08-21 12:55:50 +0200

    Update and save using VSCode API https://chat.openai.com/share/734b7d59-bd90-408c-85a1-37dec5e6ed4c

--- change.sh
+++ change.sh
@@ -1,66 +1,28 @@
 #!/bin/sh
 set -e
-goal="Consolidate file writing logic"
+goal="Update and save using VSCode API"
 echo "Plan:"
-echo "1. Modify writeAttention.ts to only call writePromptFile."
-echo "2. Modify writePromptFile.ts to handle both file writing and editor content updating."
+echo "1. Revise writePromptFile.ts to save the document unconditionally."
 
-# Step 1: Modify writeAttention.ts to only call writePromptFile.
-cat > integrations/vscode/src/writeAttention.ts << 'EOF'
-import * as vscode from 'vscode';
-import * as yaml from 'js-yaml';
-import { getRootWorkspace } from './getRootWorkspace';
-import { getPromptFilePath } from './getPromptFilePath';
-import { getCurrentOpenDocuments } from './getCurrentOpenDocuments';
-import { readPromptFile } from './readPromptFile';
-import { filterAttentionExcludes } from './filterAttentionExcludes';
-import { writePromptFile } from './writePromptFile';
-import { PromptFile } from './types';
-
-export const writeAttention = async () => {
-    const rootFolder = getRootWorkspace();
-    if (!rootFolder) {
-        return;
-    }
-
-    const promptFilePath = getPromptFilePath(rootFolder);
-    const excludeList = vscode.workspace.getConfiguration('junior').get('attentionExcludeList', []);
-    try {
-        if (promptFilePath) {
-            const currentWindows = getCurrentOpenDocuments(rootFolder);
-            const attentionSection = filterAttentionExcludes(currentWindows, excludeList, rootFolder);
-            const promptFile: PromptFile = await readPromptFile(promptFilePath);
-            promptFile.attention = attentionSection;
-            writePromptFile(promptFilePath, promptFile);
-        } else {
-            vscode.window.showErrorMessage('No prompt.yaml file found in the project root!');
-        }
-    } catch (error) {
-        vscode.window.showErrorMessage('Error updating the prompt.yaml file!');
-    }
-};
-EOF
-
-# Step 2: Modify writePromptFile.ts to handle both file writing and editor content updating.
+# Step 1: Modify writePromptFile.ts to save the document unconditionally.
 cat > integrations/vscode/src/writePromptFile.ts << 'EOF'
-import * as fs from 'fs';
 import * as vscode from 'vscode';
 import * as yaml from 'js-yaml';
 
-export const writePromptFile = (filePath: string, data: any) => {
-    const openedDocument = vscode.workspace.textDocuments.find(doc => doc.fileName === filePath);
-    if (openedDocument) {
-        const edit = new vscode.WorkspaceEdit();
-        const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(openedDocument.lineCount, 0));
-        edit.replace(openedDocument.uri, range, yaml.dump(data));
-        vscode.workspace.applyEdit(edit);
-    } else {
-        fs.writeFileSync(filePath, yaml.dump(data), 'utf8');
+export const writePromptFile = async (filePath: string, data: any) => {
+    let openedDocument = vscode.workspace.textDocuments.find(doc => doc.fileName === filePath);
+    if (!openedDocument) {
+        openedDocument = await vscode.workspace.openTextDocument(filePath);
     }
+    
+    const edit = new vscode.WorkspaceEdit();
+    const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(openedDocument.lineCount, 0));
+    edit.replace(openedDocument.uri, range, yaml.dump(data));
+    await vscode.workspace.applyEdit(edit);
+    
+    // Save the document unconditionally.
+    openedDocument.save();
 };
 EOF
-
-# We don't have the content of updateEditorContents.ts, but we know it should be deleted.
-rm integrations/vscode/src/updateEditorContents.ts
 
 echo "\033[32mDone: $goal\033[0m\n"
--- integrations/vscode/src/writePromptFile.ts
+++ integrations/vscode/src/writePromptFile.ts
@@ -1,15 +1,17 @@
-import * as fs from 'fs';
 import * as vscode from 'vscode';
 import * as yaml from 'js-yaml';
 
-export const writePromptFile = (filePath: string, data: any) => {
-    const openedDocument = vscode.workspace.textDocuments.find(doc => doc.fileName === filePath);
-    if (openedDocument) {
-        const edit = new vscode.WorkspaceEdit();
-        const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(openedDocument.lineCount, 0));
-        edit.replace(openedDocument.uri, range, yaml.dump(data));
-        vscode.workspace.applyEdit(edit);
-    } else {
-        fs.writeFileSync(filePath, yaml.dump(data), 'utf8');
+export const writePromptFile = async (filePath: string, data: any) => {
+    let openedDocument = vscode.workspace.textDocuments.find(doc => doc.fileName === filePath);
+    if (!openedDocument) {
+        openedDocument = await vscode.workspace.openTextDocument(filePath);
     }
+    
+    const edit = new vscode.WorkspaceEdit();
+    const range = new vscode.Range(new vscode.Position(0, 0), new vscode.Position(openedDocument.lineCount, 0));
+    edit.replace(openedDocument.uri, range, yaml.dump(data));
+    await vscode.workspace.applyEdit(edit);
+    
+    // Save the document unconditionally.
+    openedDocument.save();
 };
