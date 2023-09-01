a3805006df112965f9032ce98dbd0afd8f134b17
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-01 18:04:17 +0200

    Implement case-insensitive search by breaking filter into words https://chat.openai.com/share/d9b45970-fcc9-4618-ba37-8b01debab94f

--- prompt/history/2023/09/01/18:04_Implement case-insensitive search by breaking filter into words/change.sh
+++ prompt/history/2023/09/01/18:04_Implement case-insensitive search by breaking filter into words/change.sh
@@ -1,3 +1,13 @@
+#!/bin/sh
+set -e
+goal="Implement case-insensitive search by breaking filter into words"
+echo "Plan:"
+echo "1. Modify the getComparison function to split the filter into words."
+echo "2. Convert both item and filter words to lowercase for case-insensitive comparison."
+echo "3. Return true if any of the words from the filter are included in the item."
+
+# Step 1 and 2: Modify the getComparison function
+cat > src/frontend/services/helpers/getComparison.js << 'EOF'
 const getComparison = () => {
   return (item, filter) => {
     // GOAL OF THE CHANGE: Split the filter into words
@@ -12,3 +22,6 @@ const getComparison = () => {
 };
 
 export default getComparison;
+EOF
+
+echo "\033[32mDone: $goal\033[0m\n"
