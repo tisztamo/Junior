53e769b5455eaa8e37453ff1db8bd802c88754c5
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-04 20:48:25 +0200

    Add first filtered item to result set on enter press https://chat.openai.com/share/b5843018-4fc8-4218-8729-9b6e1b5bc5cb

--- src/frontend/components/MultiSelect/MultiSelect.jsx
+++ src/frontend/components/MultiSelect/MultiSelect.jsx
@@ -7,15 +7,21 @@ import computeFilteredList from './computeFilteredList';
 const MultiSelect = (props) => {
   const [searchValue, setSearchValue] = createSignal('');
   const [filteredList, setFilteredList] = createSignal([]);
+  const [selectedItems, setSelectedItems] = createSignal([...props.selectedItems]);
 
   createEffect(() => {
     setFilteredList(computeFilteredList(searchValue(), props.availableItems, props.compare, props.itemCount));
   });
 
+  const addFirstFilteredItem = () => {  // Add the first item from the filtered list to selected items
+    const updatedItems = [filteredList()[0], ...selectedItems()];
+    setSelectedItems(updatedItems);
+  };
+
   return (
     <div class="w-full bg-emphasize text-emphasize p-4 rounded border border-border">
-      <ResultSet items={props.selectedItems} />
-      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} />
+      <ResultSet items={selectedItems()} />
+      <SearchBar searchValue={searchValue} setSearchValue={setSearchValue} addFirstFilteredItem={addFirstFilteredItem} />
       <FilteredList items={filteredList()} />
     </div>
   );
--- src/frontend/components/MultiSelect/SearchBar.jsx
+++ src/frontend/components/MultiSelect/SearchBar.jsx
@@ -3,12 +3,19 @@ const SearchBar = (props) => {
     props.setSearchValue(e.target.value);
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      props.addFirstFilteredItem();
+    }
+  };
+
   return (
     <input 
       type="text"
       placeholder="Enter space-separated search terms!"
       value={props.searchValue()}
       onInput={handleInputChange}
+      onKeyPress={handleKeyPress}  // Detect key press
       class="border rounded p-2 w-full bg-emphasize text-emphasize"
     />
   );
