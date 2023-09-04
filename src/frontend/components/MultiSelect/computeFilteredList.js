36b5917db8b5687fb6417d04b173fbf82c354c65
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-04 22:44:28 +0200

    Remove selected items from filtered list https://chat.openai.com/share/e83b0308-ca14-47f0-9259-a97f554e088e

--- src/frontend/components/MultiSelect/computeFilteredList.js
+++ src/frontend/components/MultiSelect/computeFilteredList.js
@@ -1,7 +1,7 @@
-export default function computeFilteredList(searchValue, items, compare, itemCount) {
+export default function computeFilteredList(searchValue, items, compare, itemCount, selectedItems = []) {
   const comparisonFunction = compare || ((item, filter) => item.includes(filter) ? filter.length : 0);
   return items
-    .filter(item => comparisonFunction(item, searchValue) > 0)
+    .filter(item => !selectedItems.includes(item) && comparisonFunction(item, searchValue) > 0)
     .sort((a, b) => comparisonFunction(b, searchValue) - comparisonFunction(a, searchValue))
     .slice(0, itemCount);
 }
