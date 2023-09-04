1d477db8de5c7efef18502c4091894341593e649
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-04 21:36:31 +0200

    Implement scrolling for FilteredListItem https://chat.openai.com/share/803433ed-af32-44da-aaee-85c18b63ea7c

--- prompt/history/2023/09/04/21:32_Implement scrolling for FilteredListItem/change.sh
+++ prompt/history/2023/09/04/21:32_Implement scrolling for FilteredListItem/change.sh
@@ -1,3 +1,14 @@
+#!/bin/sh
+set -e
+
+goal="Implement scrolling for FilteredListItem"
+echo "Plan:"
+echo "1. Modify FilteredListItem to separate the spans with margin."
+echo "2. Add CSS to allow scrolling only for the item span."
+echo "3. Implement JS functionality to auto-scroll the item span when the item prop changes."
+
+# Step 1 and 2: Modify FilteredListItem
+cat > src/frontend/components/MultiSelect/FilteredListItem.jsx <<EOF
 import { createEffect } from "solid-js";
 
 const FilteredListItem = (props) => {
@@ -18,3 +29,6 @@ const FilteredListItem = (props) => {
 };
 
 export default FilteredListItem;
+EOF
+
+echo "\033[32mDone: $goal\033[0m\n"
--- prompt/projectSpecifics.md
+++ prompt/projectSpecifics.md
@@ -5,5 +5,3 @@
 - Use *ES6 imports*!
 - Prefer *async/await* over promises!
 - The frontend uses *Solidjs* and Tailwind, edit .jsx files accordingly!
-- Remove _all existing comments_ from the code!
-- Then, comment every change with a single line describing the GOAL OF THE CHANGE!
