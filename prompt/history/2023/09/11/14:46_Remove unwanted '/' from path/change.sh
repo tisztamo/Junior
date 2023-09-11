7a511d60c23cbde0e5f655381846ed979f053b0b
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-11 14:47:39 +0200

    Remove unwanted '/./' from path https://chat.openai.com/share/c5302eed-f675-4072-aac4-45e3ffcd4229

--- prompt/history/2023/09/11/14:46_Remove unwanted '/' from path/change.sh
+++ prompt/history/2023/09/11/14:46_Remove unwanted '/' from path/change.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+set -e
+goal="Remove unwanted '/./' from path"
+echo "Plan:"
+echo "1. Modify the flattenPaths function in src/frontend/service/helpers/flattenPaths.js to avoid adding '/./' to paths."
+
+cat > src/frontend/service/helpers/flattenPaths.js << 'EOF'
 const flattenPaths = (node, path = '') => {
   if (node.type === 'file') {
     return [path ? `${path}/${node.name}` : node.name];
@@ -11,3 +18,6 @@ const flattenPaths = (node, path = '') => {
 };
 
 export default flattenPaths;
+EOF
+
+echo "\033[32mDone: $goal\033[0m\n"
