d8f0303d004826d34aeed4eed2838285713fef01
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-18 21:25:12 +0200

    Enhance WebSocket creation with optional path https://chat.openai.com/share/fbd94467-45be-4bf5-8dfb-0ed90fcb4296

--- src/frontend/service/terminal/terminalConnection.js
+++ src/frontend/service/terminal/terminalConnection.js
@@ -1,6 +1,6 @@
 import { createWebSocket } from '../createWebSocket';
 
-const socket = createWebSocket();
+const socket = createWebSocket('/terminal');
 
 const sendDataToTerminal = (data) => {
   if (socket.readyState === WebSocket.OPEN) {
