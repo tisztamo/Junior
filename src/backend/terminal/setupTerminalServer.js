0d9a2b2c758fc10d9bd4a3dc093be7cd105f1b97
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-19 09:54:05 +0200

    Fix the terminal WebSocket implementation https://chat.openai.com/share/17c3e191-ba51-40a7-9fa4-a3889fd1ccfe

--- src/backend/terminal/setupTerminalServer.js
+++ src/backend/terminal/setupTerminalServer.js
@@ -1,21 +1,27 @@
 import { spawn } from 'child_process';
 
 export default function setupTerminalServer(socket) {
+  console.log("Setting up terminal server...");
+
   const shell = spawn('/bin/sh');
 
-  socket.on('data', (data) => {
+  socket.on('message', (data) => {
+    console.log("Received message:", data.toString());
     shell.stdin.write(data);
   });
 
   shell.stdout.on('data', (data) => {
-    socket.write(data);
+    console.log("Shell output:", data.toString());
+    socket.send(data);
   });
 
   shell.stderr.on('data', (data) => {
-    socket.write(data);
+    console.log("Shell error output:", data.toString());
+    socket.send(data);
   });
 
   shell.on('exit', () => {
-    socket.end();
+    console.log("Shell exited");
+    socket.close();
   });
 }
