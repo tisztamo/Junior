bf83876e29d9e4f43369d5c7c7aae9012e6633de
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-09-22 16:36:16 +0200

    Integrate node-pty for terminal spawning https://chat.openai.com/share/8ae7c8c2-651a-46d6-9ab4-44939125ca15

--- prompt/history/2023/09/22/16:35_Integrate node-pty for terminal spawning/change.sh
+++ prompt/history/2023/09/22/16:35_Integrate node-pty for terminal spawning/change.sh
@@ -1,3 +1,18 @@
+#!/bin/sh
+set -e
+goal="Integrate node-pty for terminal spawning"
+echo "Plan:"
+echo "1. Install node-pty"
+echo "2. Modify setupTerminalServer.js to use node-pty"
+echo "3. Ensure cross-platform terminal spawning"
+echo "4. Update terminal output and input handlers"
+
+# 1. Install node-pty
+echo "Installing node-pty..."
+npm install node-pty
+
+# 2. Modify setupTerminalServer.js to use node-pty
+cat > ./src/backend/terminal/setupTerminalServer.js << 'EOF'
 import os from 'os';
 import pty from 'node-pty';
 
@@ -25,3 +40,6 @@ export default function setupTerminalServer(socket) {
     socket.close();
   });
 }
+EOF
+
+echo "\033[32mDone: $goal\033[0m\n"
