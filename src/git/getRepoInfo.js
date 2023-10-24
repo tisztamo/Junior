3297e95a7ac086323264e99e50a13b8b4f5da0d2
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-10-24 12:11:01 +0200

    Remove unused code manually

--- src/git/getRepoInfo.js
+++ src/git/getRepoInfo.js
@@ -1,10 +1,6 @@
 import simpleGit from 'simple-git';
 import fs from 'fs';
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const git = simpleGit();
 
