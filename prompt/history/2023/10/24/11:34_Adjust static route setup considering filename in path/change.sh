db1a003cee51a15ce787fde7c63d04077c9c4dde
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-10-24 11:37:31 +0200

    Adjust static route setup considering filename in path https://chat.openai.com/share/6e5d54bb-4412-4c10-bfac-758ba3b1f524

--- prompt/history/2023/10/24/11:34_Adjust static route setup considering filename in path/change.sh
+++ prompt/history/2023/10/24/11:34_Adjust static route setup considering filename in path/change.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+set -e
+goal="Adjust static route setup considering filename in path"
+echo "Plan:"
+echo "1. Modify setupStaticRoutes.js to adjust the path by removing the filename and then determine the projectRoot"
+
+cat > ./src/backend/routes/setupStaticRoutes.js << 'EOF'
 import path from 'path';
 import express from 'express';
 
@@ -11,3 +18,6 @@ export function setupStaticRoutes(app) {
     app.use('/', express.static(frontendDistPath));
 }
 
+EOF
+
+echo "\033[32mDone: $goal\033[0m\n"
