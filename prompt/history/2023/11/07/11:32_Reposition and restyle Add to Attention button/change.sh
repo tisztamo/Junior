dba2a7385f07ba295c3b3a81a8c47e03e6e5aaee
Krisztián Schäffer <tisztamo@users.noreply.github.com>
2023-11-07 11:32:48 +0100

    Reposition and restyle Add to Attention button https://chat.openai.com/share/2c91bed0-7e8a-4d63-ba7c-13eb0907101e

--- prompt/history/2023/11/07/11:32_Reposition and restyle Add to Attention button/change.sh
+++ prompt/history/2023/11/07/11:32_Reposition and restyle Add to Attention button/change.sh
@@ -1,3 +1,11 @@
+#!/bin/sh
+set -e
+goal="Reposition and restyle Add to Attention button"
+echo "Plan:"
+echo "1. Modify the layout in FileViewerHeader.jsx to adjust button positions and padding."
+echo "2. Implement a plus sign icon for the Add to Attention button with appropriate alt text."
+echo "3. Update the onClick event to close the viewer after adding to attention."
+cat > ./src/frontend/components/files/FileViewerHeader.jsx << 'EOF'
 import ListItem from '../MultiSelect/ListItem';
 import { useAttention } from '../../model/useAttention'; // Fixed import
 
@@ -34,3 +42,5 @@ const FileViewerHeader = (props) => {
 };
 
 export default FileViewerHeader;
+EOF
+echo "\033[32mDone: $goal\033[0m\n"
